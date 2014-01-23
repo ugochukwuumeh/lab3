777e9f582476a9625f774c0607f98e751afc9794
Ugochukwu Umeh <uumeh@ucsd.edu>
2014-01-23 10:38:05 -0800

    Testing commit outside vagrant

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -41,7 +41,6 @@ function updateProject(e) {
    $(projectID).animate({
       width: $('#width').val()
    });
-
    var newText = $('#description').val();
    $(projectID + " .project-description").text(newText);
 }
